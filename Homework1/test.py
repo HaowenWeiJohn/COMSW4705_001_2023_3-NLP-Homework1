36eaccd8562af9e75883af9f15ec54039ee4cc01
ApocalyVec <59973823+HaowenWeiJohn@users.noreply.github.com>
2023-09-24 01:29:55 -0400

    update all

---
+++ Homework1/test.py
@@ -0,0 +1,33 @@
+
+
+def get_ngrams(sequence, n):
+    """
+    COMPLETE THIS FUNCTION (PART 1)
+    Given a sequence, this function should return a list of n-grams, where each n-gram is a Python tuple.
+    This should work for arbitrary values of n >= 1
+
+    #     >>> get_ngrams(["natural","language","processing"],1)
+    # [('START',), ('natural',), ('language',), ('processing',), ('STOP',)]
+    # >>> get_ngrams(["natural","language","processing"],2)
+    # ('START', 'natural'), ('natural', 'language'), ('language', 'processing'), ('processing', 'STOP')]
+    # >>> get_ngrams(["natural","language","processing"],3)
+    # [('START', 'START', 'natural'), ('START', 'natural', 'language'), ('natural', 'language', 'processing'), ('language', 'processing', 'STOP')]
+
+    """
+    sequence_pad = ["START"] + sequence + ["STOP"]
+    if n > 2:
+        sequence_pad = ["START"]*(n-2) + sequence_pad
+
+    ngrams = []
+    for i in range(len(sequence_pad)-n+1):
+        ngrams.append(tuple(sequence_pad[i:i+n]))
+
+
+
+    return ngrams
+
+
+sequence = ["natural","language","processing"]
+n = 3
+
+print(get_ngrams(sequence, n))
