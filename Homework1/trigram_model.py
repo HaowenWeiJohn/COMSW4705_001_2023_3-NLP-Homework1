bbb87db92c02b9d046b16ffc0229a71dfb7f9975
ApocalyVec <59973823+HaowenWeiJohn@users.noreply.github.com>
2023-09-25 01:26:08 -0400

    UPDATE ALL

--- Homework1/trigram_model.py
+++ Homework1/trigram_model.py
@@ -99,6 +99,7 @@ class TrigramModel(object):
                     self.trigramcounts[ngram] = 1
 
         # remove "START" from unigramcounts
+
         if ("START",) in self.unigramcounts:
             del self.unigramcounts[("START",)]
 
@@ -125,7 +126,8 @@ class TrigramModel(object):
             c_uv = self.bigramcounts[trigram[0:2]]
 
         if c_uv == 0:
-            return self.raw_unigram_probability((trigram[-1],)) # return unigram probability of the last word in trigram if trigram not in trigramcounts
+            return 1/len(self.lexicon)
+
         else:
             return c_uvw/c_uv
 
@@ -141,10 +143,11 @@ class TrigramModel(object):
         c_uw = 0
         c_u = 0
 
+
         if bigram in self.bigramcounts:
             c_uw = self.bigramcounts[bigram]
-        if bigram[0] in self.unigramcounts:
-            c_u = self.unigramcounts[bigram[0]]
+        if bigram[0:1] in self.unigramcounts:
+            c_u = self.unigramcounts[bigram[0:1]]
 
         if c_u == 0:
             return self.raw_unigram_probability((bigram[-1],))
@@ -165,6 +168,9 @@ class TrigramModel(object):
         # can be slow! You might want to compute the total number of words once,
         # store in the TrigramModel instance, and then re-use it.
 
+        # if unigram == ('START',) or unigram == ('STOP',):
+        #     return 0.0
+
         if unigram in self.unigramcounts:
             return self.unigramcounts[unigram]/self.total_number_of_tokens
         else:
@@ -181,12 +187,15 @@ class TrigramModel(object):
     def smoothed_trigram_probability(self, trigram):
         """
         COMPLETE THIS METHOD (PART 4)
-        Returns the smoothed trigram probability (using linear interpolation). 
+        Returns the smoothed trigram probability (using linear interpolation).
         """
-        lambda1 = 1/3.0
-        lambda2 = 1/3.0
-        lambda3 = 1/3.0
-        return 0.0
+        lambda1 = 1 / 3.0
+        lambda2 = 1 / 3.0
+        lambda3 = 1 / 3.0
+
+        p = lambda1 * self.raw_trigram_probability(trigram) + lambda2 * self.raw_bigram_probability(trigram[1:]) + lambda3 * self.raw_unigram_probability(trigram[2:])
+
+        return p
         
     def sentence_logprob(self, sentence):
         """
@@ -197,11 +206,14 @@ class TrigramModel(object):
         log_prob = 0.0
 
         # sentence is a list of tokens
-        for i in range(2, len(sentence) - 1):
 
-            trigram = (sentence[i - 2], sentence[i - 1], sentence[i])
+        # trigram = get_ngrams(sentence, 3)
+
+        for trigram in get_ngrams(sentence, 3): # loop through all trigrams in sentence
+
             trigram_prob = self.smoothed_trigram_probability(trigram)
             log_prob += math.log2(trigram_prob)
+
 
         return log_prob # return log probability of sentence
 
@@ -211,7 +223,31 @@ class TrigramModel(object):
         COMPLETE THIS METHOD (PART 6) 
         Returns the log probability of an entire sequence.
         """
-        return float("inf") 
+        unigramcounts = {}
+
+        log_prob = 0.0
+        for sentence in corpus:
+            for ngram in get_ngrams(sentence, 1):
+                if ngram in unigramcounts:
+                    unigramcounts[ngram] += 1
+                else:
+                    unigramcounts[ngram] = 1
+
+            log_prob += self.sentence_logprob(sentence)
+
+            # remove "START" from unigramcounts
+
+            if ("START",) in unigramcounts:
+                del unigramcounts[("START",)]
+
+
+
+        M = sum(unigramcounts.values()) # total number of tokens in corpus
+
+        l = 1/M * log_prob
+
+        return 2**(-l) # return perplexity of corpus
+
 
 
 def essay_scoring_experiment(training_file1, training_file2, testdir1, testdir2):
@@ -223,35 +259,58 @@ def essay_scoring_experiment(training_file1, training_file2, testdir1, testdir2)
         correct = 0       
  
         for f in os.listdir(testdir1):
-            pp = model1.perplexity(corpus_reader(os.path.join(testdir1, f), model1.lexicon))
+            pp1 = model1.perplexity(corpus_reader(os.path.join(testdir1, f), model1.lexicon))
+            pp2 = model2.perplexity(corpus_reader(os.path.join(testdir1, f), model2.lexicon))
+            if pp1 < pp2:
+                correct += 1
+            total += 1
+
+
             # .. 
     
         for f in os.listdir(testdir2):
-            pp = model2.perplexity(corpus_reader(os.path.join(testdir2, f), model2.lexicon))
-            # .. 
+            pp2 = model2.perplexity(corpus_reader(os.path.join(testdir2, f), model2.lexicon))
+            pp1 = model1.perplexity(corpus_reader(os.path.join(testdir2, f), model1.lexicon))
+
+            if pp2 < pp1:
+                correct += 1
+            total += 1
+            # ..
+        accuracy = correct / total
         
-        return 0.0
+        return accuracy
 
 if __name__ == "__main__":
 
-    model = TrigramModel(sys.argv[1]) 
 
-    # put test code here...
-    # or run the script from the command line with 
-    # $ python -i trigram_model.py [corpus_file]
-    # >>> 
+    train_file = '../Homework1/hw1_data/brown_train.txt'
+    # model = TrigramModel(train_file)
     #
-    # you can then call methods on the model instance in the interactive 
-    # Python prompt. 
-
-    
-    # Testing perplexity: 
-    # dev_corpus = corpus_reader(sys.argv[2], model.lexicon)
+    # # put test code here...
+    # # or run the script from the command line with
+    # # $ python -i trigram_model.py [corpus_file]
+    # # >>>
+    # #
+    # # you can then call methods on the model instance in the interactive
+    # # Python prompt.
+    #
+    #
+    # # Testing perplexity:
+    # test_file = '../Homework1/hw1_data/brown_test.txt'
+    # dev_corpus = corpus_reader(test_file, model.lexicon)
     # pp = model.perplexity(dev_corpus)
     # print(pp)
 
 
-    # Essay scoring experiment: 
-    # acc = essay_scoring_experiment('train_high.txt', 'train_low.txt", "test_high", "test_low")
-    # print(acc)
+    # Essay scoring experiment:
+
+    train_high = '../Homework1/hw1_data/ets_toefl_data/train_high.txt'
+    train_low = '../Homework1/hw1_data/ets_toefl_data/train_low.txt'
+
+    test_high = '../Homework1/hw1_data/ets_toefl_data/test_high'
+    test_low = '../Homework1/hw1_data/ets_toefl_data/test_low'
+
+
+    acc = essay_scoring_experiment(train_high, train_low, test_high, test_low)
+    print(acc)
 
