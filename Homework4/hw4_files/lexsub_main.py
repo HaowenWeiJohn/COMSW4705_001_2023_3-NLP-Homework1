0724978e415b305fd740b2c2b1aa12adb3c97d8f
HaowenWeiJohn <59973823+HaowenWeiJohn@users.noreply.github.com>
2023-12-04 22:37:37 -0500

    Homework Finished

--- Homework4/hw4_files/lexsub_main.py
+++ Homework4/hw4_files/lexsub_main.py
@@ -100,12 +100,13 @@ def wn_simple_lesk_predictor(context : Context) -> str:
 
         count = [word.count() for word in synset.lemmas() if word.name().replace('_', ' ') == context.lemma] # word.count() is the frequency of the word in the corpus
 
+        # My friend helped me with this part
         for word in synset.lemmas():
             if word.name().replace('_',' ') != context.lemma:
                 # overlap, count, word count
                 normalized_frequency = 1000*overlap + 100*(count[0] if count else 0) + word.count()
-                table[(synset, word)] = normalized_frequency # this synset and this word
 
+                table[(synset, word)] = normalized_frequency # this synset and this word
     similar_pair = max(table, key=table.get) # get the pair with the highest score
     return similar_pair[1].name().replace('_', ' ') # replace for part 3
 
@@ -141,21 +142,105 @@ class BertPredictor(object):
         self.model = transformers.TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
 
     def predict(self, context : Context) -> str:
-        return None # replace for part 5
+        # Part 5
 
-    
+        # Obtain a set of candidate synonyms (for example, by calling get_candidates).
+        # Convert the information in context into a suitable masked input representation for the DistilBERT model (see example above). Make sure you store the index of the masked target word (the position of the [MASK] token).
+        # Run the DistilBERT model on the input representation.
+        # Select, from the set of wordnet derived candidate synonyms, the highest-scoring word in the target position (i.e. the position of the masked word). Return this word.
+
+        wordnet_candidates = get_candidates(context.lemma, context.pos) # convert to list
+        wordnet_candidates = list(wordnet_candidates)
+
+        context_string = context.left_context + ['[MASK]'] + context.right_context # left and right context with [MASK]
+        input_toks = self.tokenizer.encode(' '.join(context_string)) # encode
+
+        mask_index = input_toks.index(self.tokenizer.mask_token_id) # get the index of [MASK]
+
+        # input mat
+        input_mat = np.array(input_toks).reshape((1,-1))
+
+        # predict
+        outputs = self.model.predict(input_mat, verbose=0) # don't need the verbose
+
+        # get the predictions
+        predictions = outputs[0][0]
+        target_predictions = predictions[mask_index] # get the predictions of the target word
+
+        wordnet_candidates_id = [self.tokenizer.convert_tokens_to_ids(word) for word in wordnet_candidates]
+
+        # get the candidates' scores
+        wordnet_candidates_score = [target_predictions[candidate_id] for candidate_id in wordnet_candidates_id]
+        max_score_id = np.argmax(wordnet_candidates_score) # get the index of the highest score
+        return wordnet_candidates[max_score_id] # replace for part 5
+
+
+class CustomPredictor:
+    def __init__(self, filename):
+        self.model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)
+
+    def predict_nearest(self,context : Context) -> str:
+
+        # here is the approch, we first get the candidates,
+        synset_vector_dict = {}
+
+
+        for synset in wn.synsets(context.lemma, context.pos):
+            vector_sum = np.zeros(300)
+            synset_vector_dict[synset] = vector_sum
+            for lemmas in synset.lemmas():
+                if lemmas.name() != context.lemma:
+                    if lemmas.name() in self.model.key_to_index:
+                        synset_vector_dict[synset] += self.model.get_vector(lemmas.name())
+
+        # get the lemma's vector
+        target_word_vector = self.model.get_vector(context.lemma)
+
+        synset_score_dict = {}
+        for synset in synset_vector_dict:
+            synset_score_dict[synset] = np.dot(target_word_vector, synset_vector_dict[synset])
+
+        # get the highest score synset
+        highest_score_synset = max(synset_score_dict, key=synset_score_dict.get)
+
+        # lemma height score
+        lemma_score_dict = {}
+        for lemma in highest_score_synset.lemmas():
+            if lemma.name() != context.lemma:
+                if lemma.name() in self.model.key_to_index:
+                    lemma_score_dict[lemma] = self.model.similarity(context.lemma, lemma.name())
+
+        # get the highest score lemma
+        highest_score_lemma = max(lemma_score_dict, key=lemma_score_dict.get)
+        return highest_score_lemma.name()
+
+
+
+
+
+
+
+
+            # for lemma in synset.lemmas():
+            #     if lemma.name() == context.lemma:
+            #         return lemma.name()
+
+
+
+
 
 # if __name__=="__main__":
 #
 #     # At submission time, this program should run your best predictor (part 6).
 #
-#     #W2VMODEL_FILENAME = 'GoogleNews-vectors-negative300.bin.gz'
-#     #predictor = Word2VecSubst(W2VMODEL_FILENAME)
+#     W2VMODEL_FILENAME = 'GoogleNews-vectors-negative300.bin'
+#     predictor = Word2VecSubst(W2VMODEL_FILENAME)
 #
 #     for context in read_lexsub_xml(sys.argv[1]):
 #         #print(context)  # useful for debugging
 #         prediction = smurf_predictor(context)
 #         print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
+
 
 
 # Part 1
@@ -240,8 +325,56 @@ class BertPredictor(object):
 #         print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
 #
 # # Total = 298, attempted = 298
-# # precision = 0.112, recall = 0.112
 # # precision = 0.115, recall = 0.115
 # # Total with mode 206 attempted 206
 # # precision = 0.170, recall = 0.170
+
+
+
+# #
+# # # Part 5
+# #
+# if __name__=="__main__":
+#     import io
+#
+#     f = io.open('part5.predict', 'w', newline='\n')
+#     bert_predictor = BertPredictor()
+#     for context in read_lexsub_xml("lexsub_trial.xml"):
+#         #print(context)  # useful for debugging
+#         prediction = bert_predictor.predict(context)
+#         a = "{}.{} {} :: {}\n".format(context.lemma, context.pos, context.cid, prediction)
+#         f.write(a)
+#
+#         print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
+#
+#
+# # Total = 298, attempted = 298
+# # precision = 0.115, recall = 0.115
+# # Total with mode 206 attempted 206
+# # precision = 0.170, recall = 0.170
+#
+
+
+
+
+
+
+
+
+# # Part 6
+
+if __name__=="__main__":
+    import io
+
+    f = io.open('part6.predict', 'w', newline='\n')
+    custom_predictor = CustomPredictor('GoogleNews-vectors-negative300.bin')
+    for context in read_lexsub_xml("lexsub_trial.xml"):
+        #print(context)  # useful for debugging
+        prediction = custom_predictor.predict_nearest(context)
+        a = "{}.{} {} :: {}\n".format(context.lemma, context.pos, context.cid, prediction)
+        f.write(a)
+
+        print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
+
+
 
--- Homework4/test.py
+++ Homework4/test.py
@@ -1,2 +1,21 @@
-import gensim
-model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin.gz', binary=True)
+# import gensim
+# model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin.gz', binary=True)
+
+
+import transformers
+import tensorflow as tf
+import numpy as np
+
+
+model = transformers.TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
+
+
+tokenizer = transformers.DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+
+test_tokenizer = tokenizer.tokenize("If your money is tight, don't cut corners.")
+
+input_toks = tokenizer.encode("If your money is tight, don't cut corners")
+
+result = tokenizer.convert_ids_to_tokens(input_toks)
+
+input_mat = np.array(input_toks).reshape((1,-1))
